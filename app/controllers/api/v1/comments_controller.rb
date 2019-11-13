638dd3e700350701ec7988f300a341a801aa1189
github username <github email address>
2019-11-13 13:48:29 -0500

    comment post made

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -10,6 +10,17 @@ module Api
                 comment = Comment.find(params[:id])
                 render json: comment
             end
+
+            def create
+                comment = Comment.create(comment_params)
+                render json: comment
+            end
+
+            private
+
+            def comment_params
+                params.require(:comment).permit(:text, :rating, :image_url, :author, :recipe_id)
+            end
         end
     end
 end
